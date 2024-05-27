e62b87485eff7a593dce840ccbd18cad58a9596e
deveshcloud <deveshthaware2@gmail.com>
2024-05-27 16:21:27 +0530

    Update variable.tf

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,31 +1,14 @@
-# modules/ec2_instance/variables.tf
 variable "ami" {
-  description = "The ID of the AMI to use for the instance"
-  type        = string
+    type = string
+  
 }
 
 variable "instance_type" {
-  description = "The type of instance to start"
-  type        = string
+    type = string
+  
 }
 
-variable "key_name" {
-  description = "The key name to use for the instance"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet to launch the instance in"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "The name of the instance"
-  type        = string
-}
-
-variable "tags" {
-  description = "A map of tags to add to the instance"
-  type        = map(string)
-  default     = {}
+variable "subnetid" {
+    type = string
+  
 }
