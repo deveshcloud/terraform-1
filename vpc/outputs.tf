166ffbf71a2ec941fca482e802f8adc1441a9c33
deveshcloud <deveshthaware2@gmail.com>
2024-05-27 16:25:29 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,10 +2,7 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnets" {
-  value = aws_subnet.private[*].id
+# Output the Subnet ID
+output "public_subnet_id" {
+  value = aws_subnet.public.id
 }
