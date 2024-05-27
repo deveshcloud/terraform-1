239d36c615c777fbf560d68b748c27c73debbfe9
deveshcloud <deveshthaware2@gmail.com>
2024-05-27 16:26:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,33 @@
-# Module for IAM Role
-module "iam" {
-  source = "./iam"
-  image_id = var.image_id
-  ec2_instance_type = var.ec2_instance_type  
+module "s3" {
+  source      = "./s3"
+  bucket_name = var.bucket
 }
 
 
+
+
+
+module "iam" {
+  source     = "./iam"
+  role       = var.role_name
+  depends_on = [module.s3]
+
+}
+
+
+module "vpc" {
+  source = "./vpc"
+  cidr = var.cidr
+  vpc_name = var.vpc_name
+  public_subnet_cidr = var.public_subnet_cidr
+  public_subnet_name = var.public_subnet_name
+}
+
+module "ec2" {
+  source = "./ec2"
+  subnetid = module.vpc.public_subnet_id
+  ami = "ami-07ef4004db979fcd4"
+  instance_type = "t2.micro"
+
+  
+}
