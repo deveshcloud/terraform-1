4bf2484459803ce804c75b4c59c5096a856b5492
deveshcloud <deveshthaware2@gmail.com>
2024-05-27 11:33:35 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,9 @@
-variable "bucket" {
-  type = map(object({
-    bucket_name = string
-  }))
+variable "ec2_instance_type" {
+  description = "Instance type to crate the resources"
+  type = string   
 }
 
-
-variable "aws_iam_user" {
-    type = map(object({
-      iam_user_name = string
-    }))
-  
+variable "image_id" {
+  description = "Image AMI to create the resources"
+  type = string
 }
